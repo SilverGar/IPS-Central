33b75fb85b834dac85b2dad3b1f2cf46faf1fe7f
A00828146 <A00828146@itesm.mx>
2022-04-09 13:26:28 -0500

    Initial Setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,49 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MsalGuard } from '@azure/msal-angular';
 
-const routes: Routes = [];
+//Superusuario
+
+
+//Recursos humanos
+
+
+//Empleado
+import { HomeComponent as EmployeeHome } from './components/employee/home/home.component';
+import { DetailsComponent as EmployeeDetailsComponent } from './components/employee/details/details.component';
+
+
+//Shared
+import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
+
+const routes: Routes = [
+  
+  {
+    path: 'employee_home', 
+    component: EmployeeHome,
+    canActivate: [MsalGuard]
+  },
+  {
+    path: 'employee_details',
+    component: EmployeeDetailsComponent,
+    canActivate: [MsalGuard]
+  },
+  {
+    path: 'sidebar',
+    component: SidebarComponent,
+    canActivate: [MsalGuard]
+  }
+]
+
+const isIFrame = window !== window.parent && !window.opener;
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes, {
+      initialNavigation: !isIFrame ? 'enabled' : 'disabled'
+    })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
+import { HomeComponent } from './components/employee/home/home.component';
+import { DetailsComponent } from './components/employee/details/details.component';
+import { LoadFileComponent } from './components/superuser/load-file/load-file.component';
+import { DashboardComponent } from './components/humanresources/dashboard/dashboard.component';
+import { NotificationsComponent } from './components/humanresources/notifications/notifications.component';
+import { SignInComponent } from './components/shared/sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HomeComponent,
+    DetailsComponent,
+    LoadFileComponent,
+    DashboardComponent,
+    NotificationsComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
