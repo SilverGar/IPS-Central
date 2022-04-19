a974ab0b5fc029a6c385039b88f0a892464ed4c7
SilverGar <a00829450@itesm.mx>
2022-04-19 17:09:06 -0500

    Agregue carpeta manage users

---
+++ src/app/components/superuser/manage-users/manage-users.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-users',
+  templateUrl: './manage-users.component.html',
+  styleUrls: ['./manage-users.component.css']
+})
+export class MANAGEUSERSComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  // Variables
+
+}
