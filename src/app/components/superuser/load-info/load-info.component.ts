a03f55d77f0ad8fb801b920a163d518600ce59a2
SilverGar <a00829450@itesm.mx>
2022-04-29 17:02:04 -0500

    agregue carpeta load-info

---
+++ src/app/components/superuser/load-info/load-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-load-info',
+  templateUrl: './load-info.component.html',
+  styleUrls: ['./load-info.component.css']
+})
+export class LoadInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
