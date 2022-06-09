4dea410738c0eb4d7e48401e8d6e0fe685e239c7
A00828146 <A00828146@itesm.mx>
2022-06-08 23:37:58 -0500

    Visualizador de equipos independiente y funcional para Superusuario, ahora solo puede visualizar equipos, no puede aprobar o ver conflictos

--- src/app/components/superuser/superuser-visualize-teams/superuser-visualize-teams.component.ts
+++ src/app/components/superuser/superuser-visualize-teams/superuser-visualize-teams.component.ts
@@ -1,6 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Complete_Team360, DbUserTeam360, User } from 'src/app/models/db-user';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { Complete_Team360, DbUserTeam360, getConflictData, NotificationData, User } from 'src/app/models/db-user';
 import { DatabaseService } from 'src/app/services/dataManagement/database.service';
+import { HrPopUpConflictComponent } from '../../humanresources/hr-pop-up-conflict/hr-pop-up-conflict.component';
 
 @Component({
   selector: 'app-superuser-visualize-teams',
@@ -9,53 +12,110 @@ import { DatabaseService } from 'src/app/services/dataManagement/database.servic
 })
 export class SuperuserVisualizeTeamsComponent implements OnInit {
   userList?: Array<User>
+  displayUserList?: Array<User> 
   userTeam?: Array<Complete_Team360>
   userTeamNotApproved?: Array<Complete_Team360>
   currentUser: string = ""
-  displayPage: boolean = false
+  currentUserMail: string = ""
 
+  searchQuery: string = ''
+
+  ownerEditing: boolean = false
+
+  //Display Filters
+  alphabeticSort: boolean = false
+  approvedSort: boolean = false
+  orphanSort: boolean = false
 
   constructor(
-    private db: DatabaseService
+    private db: DatabaseService,
+		private dialog: MatDialog,
+    private http: HttpClient
   ) { }
 
   ngOnInit(): void {
+    this.queryUsers()
+  }
+
+  queryUsers() {
     this.db.getUsers().subscribe(resp =>{
       this.userList = resp
-      this.displayPage = true
+      this.displayUserList = resp
+      this.alphabeticSort = false
+      this.approvedSort = false
     })
   }
 
+  checkConflict(input: Complete_Team360){
+      const dialogConfig = new MatDialogConfig()
+      dialogConfig.disableClose = true
+      dialogConfig.autoFocus = true
 
-  getTeam(input: string){
-    this.db.getCompleteTeam360(input).subscribe(resp => {
+			dialogConfig.data = {
+				newUser: input
+			}
 
-      var newUserList: Array<Complete_Team360> = []
-      var newUserListNotApproved: Array<Complete_Team360> = []
-      for(var i in resp){
-        
-        if(resp[i].OwnerCheck == null){
-          resp[i].OwnerCheck = true
-        }
-        if(resp[i].PartnerCheck == null){
-          resp[i].PartnerCheck = true
-        }
+			const dialogRef = this.dialog.open(HrPopUpConflictComponent, dialogConfig)
+			dialogRef.afterClosed().subscribe(data => {
+        input.Notification = data
+			})
 
-        if(resp[i].Approved == false){
-          newUserListNotApproved.push(resp[i])
+  }
+
+  sortUserList(input: number, query: string){
+    if(this.userList != null){
+      switch (input){
+        case 1:
+          this.displayUserList = this.filterItems(query)
+          break
+        case 2:
+          this.displayUserList?.sort((a, b) => (b.name ?? '').localeCompare((a.name ?? '')))
+          this.alphabeticSort = !this.alphabeticSort
+          break
+        case 3:
+          this.displayUserList?.sort((a, b) => (a.name ?? '').localeCompare((b.name ?? '')))
+          this.alphabeticSort = !this.alphabeticSort
+          break;
+        case 5:
+          this.orphanSort = !this.orphanSort
+          this.displayUserList = this.filterOrphans(this.orphanSort)
+          break;
+          
+      }
+    }
+  }
+
+  filterOrphans(input: boolean){
+    var newDisplay: Array<User> = []
+    if(this.userList != null){
+      for(var i in this.userList){
+        if(input){
+          if(this.userList[i].Status == 0){
+            newDisplay.push(this.userList[i])
+          }
         }
         else{
-          newUserList.push(resp[i])
+          if(this.userList[i].Status != 0){
+            newDisplay.push(this.userList[i])
+          }
         }
+        
       }
+    }
+    return newDisplay
+  }
 
-      this.userTeam = newUserList
-      this.userTeamNotApproved = newUserListNotApproved
+  filterItems(query: string){
+      return this.userList?.filter(function(el) {
+          return el.name?.toLocaleLowerCase().indexOf(query.toLowerCase()) !== -1
+      })
+  }
+
+  getTeam(input: string){
+    this.currentUserMail = input
+    this.db.getCompleteTeam360(input).subscribe(resp => {
+      this.userTeam = resp
     })
-  }
 
-  debugging(input: string){
-    console.log("Hola" + input)
   }
-
 }
